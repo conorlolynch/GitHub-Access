30c3b62a9a0e4df42ef84555bf971ccc4f90ef97
conorlolynch <conor.lo.lynch556@gmail.com>
2020-11-29 17:46:50 +0000

    added language pie chart

--- github_access.py
+++ github_access.py
@@ -6,19 +6,16 @@ import time
 from datetime import datetime, date, time, timedelta
 
 
-# So potential things that can be built:
-
-# 2) Show all the langauges this user has used
-# 3) Show the times the user commits at
-
-
-
 
 app = Flask(__name__)
+
+user = None
+user_dict = None
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/", methods=['POST'])
 def my_form_post():
@@ -32,6 +29,98 @@ def index(inputvalue):
     user_dict = searchForAccount(inputvalue)
 
     return render_template("dashboard.html", content=user_dict)
+
+
+
+def getCommits(repos, user_dict):
+    try:
+        # Make sure the user actually has repositories
+        if repos != None:
+            hours_and_commits = repos[0].get_stats_punch_card().raw_data
+
+
+            # Now convert this into the desired format [hour, num_commits]
+            form = []
+            for index in range(0,len(hours_and_commits)):
+                form.append([index % 24, hours_and_commits[index][2]])
+
+
+            # Make sure they have more than one repository
+            if (repos.totalCount > 1):
+
+                # For each repository in this users account
+                for repo in repos[1:]:
+
+                    # Make sure the repo isnt private
+                    if (not repo.private):
+                        if repo.owner.login == user_dict['login']:
+                            try:
+                                data = repo.get_stats_punch_card().raw_data
+
+                                # For every index in the array
+                                for index in range(0,len(form)):
+                                    form[index][1] = form[index][1] + data[index][2]
+                            except Exception as e:
+                                pass
+
+
+
+            # Now store these separetely based on day
+            user_dict['commit'][0] = form[0:24]         # Commit by hour on Sunday
+            user_dict['commit'][1] = form[24:48]        # Commit by hour on Monday
+            user_dict['commit'][2] = form[48:72]        # Commit by hour on Tuesday
+            user_dict['commit'][3] = form[72:96]        # Commit by hour on Wednesday
+            user_dict['commit'][4] = form[96:120]       # Commit by hour on Thursday
+            user_dict['commit'][5] = form[120:144]      # Commit by hour on Friday
+            user_dict['commit'][6] = form[144:]         # Commit by hour on Saturday
+
+        else:
+            template = [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0],
+                       [12, 0], [13, 0], [14, 0], [15, 0], [16, 0], [17, 0], [18, 0], [19, 0], [20, 0], [21, 0], [22, 0], [23, 0]]
+
+            user_dict['commit'][0] = template
+            user_dict['commit'][1] = template
+            user_dict['commit'][2] = template
+            user_dict['commit'][3] = template
+            user_dict['commit'][4] = template
+            user_dict['commit'][5] = template
+            user_dict['commit'][6] = template
+
+    except Exception as e:
+        print("Could not read commits: ",e)
+
+
+def getLanguages(repositories, user_dict):
+    try:
+        temp_dict = {}
+
+        # Make sure the user has repositories
+        if (repositories != None):
+
+            # For each repository they have
+            for repo in repositories:
+
+                # Make sure the repo isnt private and this user owns it
+                if (not repo.private):
+                    if repo.owner.login == user_dict['login']:
+
+                        # Check if this repo has 1 or more programming languages used in it
+                        languages = repo.get_languages()
+                        if (languages != {} or languages != None):
+
+                            # For all the languages add on the score for this repo
+                            for lang,score in languages.items():
+                                if (lang in temp_dict):
+                                    temp_dict[lang] = temp_dict[lang] + score
+                                else:
+                                    temp_dict[lang] = score
+
+            # Take the temp dict and convert it to the desired format [['lang', score], ...]
+            for lang, score in temp_dict.items():
+                user_dict['languages'].append([str(lang),score])
+
+    except Exception as e:
+        print("\n\nCould not get languages: ",e)
 
 
 def searchForAccount(token):
@@ -56,98 +145,24 @@ def searchForAccount(token):
     user_dict['name'] = user.name
     user_dict['bio'] = user.bio
     user_dict['email'] = user.email
+    user_dict['languages'] = []
     user_dict['commit'] = {0:[], 1:[], 2:[],3:[],4:[],5:[],6:[]}
 
     # https://api.github.com/repos/conorlolynch/A-Star-Pathfinding/stats/punch_card
 
     repos = user.get_repos()
 
-    # Make sure the user actually has repositories (looking at you Jeremeie)
-    if repos != None:
-        hours_and_commits = repos[0].get_stats_punch_card().raw_data
+    getLanguages(repos, user_dict)
+    getCommits(repos, user_dict)
 
-
-        # Now convert this into the desired format [hour, num_commits]
-        form = []
-        for index in range(0,len(hours_and_commits)):
-            form.append([index % 24, hours_and_commits[index][2]])
-
-
-        print("Number of repositories: ",repos.totalCount)
-
-
-
-        # Make sure they have more than one repository
-        if (repos.totalCount > 1):
-
-            # For each repository in this users account
-            for repo in repos[1:]:
-
-                # Make sure the repo isnt private
-                if (not repo.private):
-                    if repo.owner.login == user_dict['login']:
-                        try:
-                            data = repo.get_stats_punch_card().raw_data
-
-                            # For every index in the array
-                            for index in range(0,len(form)):
-                                form[index][1] = form[index][1] + data[index][2]
-                        except Exception as e:
-                            pass
-
-
-
-        # Now store these separetely based on day
-        user_dict['commit'][0] = form[0:24]         # Commit by hour on Sunday
-        user_dict['commit'][1] = form[24:48]        # Commit by hour on Monday
-        user_dict['commit'][2] = form[48:72]        # Commit by hour on Tuesday
-        user_dict['commit'][3] = form[72:96]        # Commit by hour on Wednesday
-        user_dict['commit'][4] = form[96:120]       # Commit by hour on Thursday
-        user_dict['commit'][5] = form[120:144]      # Commit by hour on Friday
-        user_dict['commit'][6] = form[144:]         # Commit by hour on Saturday
-
-    else:
-        template = [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0],
-                   [12, 0], [13, 0], [14, 0], [15, 0], [16, 0], [17, 0], [18, 0], [19, 0], [20, 0], [21, 0], [22, 0], [23, 0]]
-
-        user_dict['commit'][0] = template
-        user_dict['commit'][1] = template
-        user_dict['commit'][2] = template
-        user_dict['commit'][3] = template
-        user_dict['commit'][4] = template
-        user_dict['commit'][5] = template
-        user_dict['commit'][6] = template
-
-
+    # print(user_dict['commit'])
+    # print(user_dict['languages'])
     return user_dict
 
     # except Exception as e:
     #     print(e)
     #     return user_dict
 
-
-# def searchForAccount(user_or_token):
-#     try:
-#         # Creating GitHub client
-#         g = Github(user_or_token)
-#         for repo in g.get_user().get_repos():
-#             print("Name: ",repo.name)
-#             print("Description: ",repo.description)
-#
-#             # Print all repository content
-#             print("Contents:")
-#             for content in repo.get_contents(""):
-#                 print(content)
-#
-#     except Exception as e:
-#         # Token didnt work so check to see if its a username instead
-#         print("Attempting Username")
-#         base_url = r"https://api.github.com/users"
-#
-#         api_url = F"{base_url}/{user_or_token}"
-#         res = requests.get(api_url)
-#         info = json.loads(res.content.decode('utf-8'))
-#         print(info)
 
 
 if __name__ == "__main__":
